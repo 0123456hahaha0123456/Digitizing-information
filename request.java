8f4ae830454a9ba55dd4ad3c6b9e54d07e217523
Tran Trung Duc <30802486+0123456hahaha0123456@users.noreply.github.com>
2018-11-02 23:42:56 +0300

    Add files via upload
    Разделить программу из лабораторной работы №5 на клиентский и серверный модули. Серверный модуль должен реализовывать все функции управления коллекцией в интерактивном режиме, кроме отображения текста в соответствии с сюжетом предметной области. Клиентский модуль должен запрашивать у сервера текущее состояние коллекции, генерировать сюжет, выводить его на консоль и завершать работу.
    
    Хранящиеся в коллекции объекты должны иметь следующие характеристики:
    
    имя, название или аналогичный текстовый идентификатор;
    размер или аналогичный числовой параметр;
    характеристику, определяющую местоположение объекта на плоскости/в пространстве;
    время/дату рождения/создания объекта.
    Если аналогичные характеристики уже есть, добавлять их не нужно.
    
    Необходимо выполнить следующие требования:
    
    Коллекцию из ЛР №5 заменить на ее потокобезопасный аналог.
    Операции обработки объектов коллекции должны быть реализованы с помощью Stream API с использованием лямбда-выражений.
    Объекты между клиентом и сервером должны передаваться в сериализованном виде.
    Объекты в коллекции, передаваемой клиенту, должны быть отсортированы по умолчанию.
    Получив запрос, сервер должен создавать отдельный поток, который должен формировать и отправлять ответ клиенту.
    Клиент должен корректно обрабатывать временную недоступность сервера.
    Обмен данными между клиентом и сервером должен осуществляться по протоколу UDP.
    На стороне сервера должен использоваться датаграммы а на стороне клиента - сетевой канал

---
+++ request.java
@@ -0,0 +1,101 @@
+import java.io.IOException;
+import java.io.Serializable;
+
+public class request implements Serializable{
+    private static final long serialVersionUID = 5129934110456581901L;
+    private String str;
+    private int typeCommand ;
+    private People people;
+    private String key;
+    public request(String str){
+        this.str = str;
+        solve();
+    }
+
+    public void setKey(String key){
+        this.key = key;
+    }
+    public String getKey(){return this.key;}
+    public People getPeople() {
+        return people;
+    }
+    public int getTypeCommand(){
+        return this.typeCommand;
+    }
+    public void setPeople(People people){
+        this.people = people;
+    }
+    private void setTypeCommand(int type){
+        this.typeCommand = type;
+    }
+    private void solve(){
+        try{
+            String _str = check(this.str);
+            if (_str.startsWith("add_if_min")) {
+                setTypeCommand(1);
+                String jsonStr = check(_str.substring(10));
+                if (jsonStr.equals(null)) setPeople(null);
+                else {
+                    People duc = loadPeople.loadNewPeople(jsonStr);
+                    setPeople(duc);
+                }
+                return;
+            }
+            if (_str.startsWith("remove_lower")){
+                setTypeCommand(2);
+                String jsonStr = check(_str.substring(12));
+                if (jsonStr.equals(null)) setPeople(null);
+                else {
+                    People duc = loadPeople.loadNewPeople(jsonStr);
+                    setPeople(duc);
+                }
+                return;
+            }
+            if (_str.startsWith("remove_all")){
+                setTypeCommand(3);
+                String jsonStr = check(_str.substring(10));
+                if (jsonStr.equals(null)) setPeople(null);
+                else {
+                    People duc = loadPeople.loadNewPeople(jsonStr);
+                    setPeople(duc);
+                }
+                return;
+            }
+            if (_str.equals("clear")){
+                setTypeCommand(4);
+                return;
+            }
+            if (_str.equals("info")){
+                setTypeCommand(5);
+                return;
+            }
+            if (_str.equals("load")){
+                setTypeCommand(6);
+                return;
+            }
+            if (_str.startsWith("remove")){
+                setTypeCommand(7);
+                if (_str.equals("remove")) { setKey(null); }
+                else {
+                    key = check(_str.substring(6));
+                    if (key.equals(null)) { setKey(null);}
+                    else setKey(key);
+                }
+            }
+            if (_str.equals("help")){
+                setTypeCommand(8);
+            }
+        }catch (Exception e){
+            this.str = "JSON format was wrong. Please write object as a json type";
+        }
+
+    }
+
+    static private String check(String tmp){
+        int i =0, j= tmp.length()-1;
+        if (i>j) return "";
+        while(tmp.charAt(i) == ' ') i++;
+        while(tmp.charAt(j) == ' ') j--;
+        return  tmp.substring(i,j+1);
+    }
+}
