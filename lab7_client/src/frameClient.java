59a1a0f68c875d5dbfacdf7e1a68371cfc50abdf
0123456hahaha0123456 <ductran.tpc@gmail.com>
2019-01-15 01:08:03 +0300

    Continue move funcion

--- lab7_client/src/frameClient.java
+++ lab7_client/src/frameClient.java
@@ -1,14 +1,13 @@
+import com.sun.org.apache.regexp.internal.RECompiler;
 import javafx.scene.control.Tooltip;
 
 import javax.print.DocFlavor;
 import javax.swing.*;
 import javax.swing.border.TitledBorder;
 import java.awt.*;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
+import java.awt.event.*;
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 public class frameClient  {
     ArrayList<Rectangle> arr;
@@ -56,15 +55,19 @@ public class frameClient  {
         panelBut.setLayout(new GridLayout(1,4));
 
         //tao filter color - Jlist
-        JList listColor = new JList(Client.colour);
+        listColor = new JList(Client.colour);
         listColor.setFont(new Font("Arial",Font.ITALIC, 18));
+        listColor.setLayoutOrientation(JList.VERTICAL);
+
+        JScrollPane listScroller = new JScrollPane(listColor);
+        listScroller.setPreferredSize(new Dimension(250, 100));
 
         JPanel listCol = new JPanel();
-        listCol.add(listColor);
+        listCol.add(listScroller);
         setTitle(listCol,"filter by color");
 
         //tao filter dien tich - Jtexfield
-        JTextField arealist = new JTextField();
+        arealist = new JTextField();
         arealist.setColumns(10);
 
         JPanel area = new JPanel();
@@ -73,22 +76,85 @@ public class frameClient  {
 
         //tao nut start
         JPanel start = new JPanel();
-        JButton startButton = new JButton();
-        startButton.setText("Start");
+        JButton startButton = new JButton("Start");
+        //startButton.setText("Start");
+        startButton.addActionListener(this::actionPerformed);
         start.add(startButton,BorderLayout.CENTER);
 
         //tao nut End
         JPanel end = new JPanel();
-        JButton endButton = new JButton();
-        endButton.setText("End");
-        //end.add(endButton,BorderLayout.CENTER);
+        JButton endButton = new JButton("End");
+        endButton.addActionListener(this::actionPerformed);
         end.add(endButton,BorderLayout.CENTER);
 
         panelBut.add(listCol);
         panelBut.add(area);
         panelBut.add(start);
         panelBut.add(end);
+    }
 
+    //the filter rec
+    private void recFilter(String listColorString, String areaString){
+        int area;
+        if (areaString!=null) area = Integer.parseInt(areaString);
+        else area = 0;
+
+        Color listColor = Color.YELLOW;
+        if (listColorString!= null) {
+            switch (listColorString) {
+                case "red":
+                    listColor = Color.RED;
+                    break;
+                case "black":
+                    listColor = Color.BLACK;
+                    break;
+                case "green":
+                    listColor = Color.GREEN;
+                    break;
+                case "magenta":
+                    listColor = Color.MAGENTA;
+                    break;
+                case "yellow":
+                    listColor = Color.YELLOW;
+                    break;
+            }
+        }
+
+        //find which rec will acceptable condition
+        for(int i=0;i<arr.size();i++){
+            Rectangle tmp = arr.get(i);
+            if ((tmp.getHeight() * tmp.getWidth() >area) && ((tmp.getColor() == listColor) || (listColorString==null))){
+                runCondition(i);
+            }
+        }
+    }
+    //sua tiep phan move
+    private void runCondition(int index){
+        for(int i =0;i<20;i++){
+            Rectangle tmp = arr.get(index);
+            tmp.setLocation(tmp.getCorX(), tmp.getCorY()-i);
+        }
+    }
+
+    private void actionPerformed(ActionEvent e){
+        if (e.getActionCommand().equals("Start")){
+            String listColorString;
+            String areaString;
+            try{
+                listColorString  = listColor.getSelectedValue().toString();
+                if (!arealist.getText().equals("")) areaString =  arealist.getText() ;
+                else areaString = "0";
+
+                if (listColorString!=null || areaString!=null) {
+                    System.out.println(listColorString + " " + areaString);
+                    recFilter(listColorString,areaString);
+                }
+            }catch (NullPointerException exp){
+                System.out.println("hihi");
+            }
+        }
+        else if (e.getActionCommand().equals("End")) {
+        }
     }
 
     private void setTitle(JPanel duc, String title){
@@ -102,6 +168,7 @@ public class frameClient  {
         Rectangle duc;
         for(int i=0;i<arr.size();i++){
             duc = arr.get(i);
+            duc = arr.get(i);
             panelPos.add(duc);
             duc.setToolTipText("x = " + duc.getCorX() +" "
                     +"y = " + duc.getCorY() + " "
@@ -112,4 +179,7 @@ public class frameClient  {
             duc.draw();
         }
     }
+
+    private JTextField arealist;
+    private JList listColor;
 }
